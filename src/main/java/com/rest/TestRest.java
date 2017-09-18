70dcbb01288a15e6ad983ffbc31303ec78edf72e
Kalathiparambil <BKALATHI@ford.com>
2017-09-18 06:16:42 -0400

    Signed-off-by: Kalathiparambil <BKALATHI@ford.com>

---
+++ src/main/java/com/rest/TestRest.java
@@ -0,0 +1,27 @@
+package com.rest;
+
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/shop")
+public class TestRest {
+
+	//http://localhost:8080/NGDSDMISSO/rest/shop/products
+	@GET
+    @Path("products")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String test() throws IOException {
+    	//http://localhost:8080/dmi/sso?dealername=A1234
+    	String result = "";
+		try {
+			result = "test";
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	return result;
+    }	
+}
